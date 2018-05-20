5b9f26050164d00a04dd33056cedd24973f7e11a
S.Maurya <30897967+mauryashashi@users.noreply.github.com>
2018-05-20 15:59:56 +0530

    Create gridCell.js
    Individual cell of the grid which finally contains items

---
+++ gridCell.js
@@ -0,0 +1,39 @@
+"use strict";
+
+    directive('gridCell', function () {
+
+        var gridCellDir = {};
+
+        gridCellDir.restrict = 'E';
+        gridCellDir.transclude = false;
+        gridCellDir.scope = { items: "=" };
+        gridCellDir.template = '<div class="panel" ng-class="{\'panel-primary\': itemSelected, \'panel-default\': !itemSelected}" >' +
+            '<div ng-if="items" class="panel-heading" ng-click="slotSelected();">' +
+            '<ul class="list-group">' +
+            '<li class="list-group-item">{{items[0]}}</li>' +
+            '<li class="list-group-item">{{items[1]}}</li>' +
+            '</ul>' +
+            '</div>' +
+            '</div>';
+
+        gridCellDir.link = function (scope, element, attributes) {
+            scope.itemSelected = false;
+			
+            scope.slotSelected = function () {
+                scope.$emit("itemSelected", scope.items);
+            }
+
+            scope.$on("itemSelected", function (event, items) {
+                if (items == scope.items) {
+                    scope.itemSelected = true;
+                } else {
+                    scope.itemSelected = false;
+                }
+                event.preventDefault();
+            });
+
+        };
+
+        return gridCellDir;
+
+    });
